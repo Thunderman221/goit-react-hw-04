be4f28701ce90a67157443a421502de611cc5d13
Thunderman221 <119857501+Thunderman221@users.noreply.github.com>
2024-09-21 16:56:12 +0300

    little changes

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -4,19 +4,15 @@ import s from "./ImageGallery.module.css";
 const ImageGallery = ({ images, onImageClick }) => {
   return (
     <ul className={s.list}>
-      {images.length > 0 ? (
-        images.map((image) => (
-          <li key={image.id} className={s.item}>
-            <ImageCard
-              src={image.urls.small}
-              alt={image.alt_description}
-              onClick={() => onImageClick(image)}
-            />
-          </li>
-        ))
-      ) : (
-        <li>No images available.</li>
-      )}
+      {images.map((image) => (
+        <li
+          key={image.id}
+          className={s.item}
+          onClick={() => onImageClick(image)}
+        >
+          <ImageCard src={image.urls.small} alt={image.alt_description} />
+        </li>
+      ))}
     </ul>
   );
 };
--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,20 +1,31 @@
 import Modal from "react-modal";
 import s from "./ImageModal.module.css";
 
-Modal.setAppElement("#root");
-
 const ImageModal = ({ isOpen, onRequestClose, image }) => {
+  if (!image) return null; // Повертаємо null, якщо немає зображення
+
+  const { user, likes, urls } = image; // Витягуємо дані з об'єкта зображення, без views
+
   return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      className={s.modal}
-      overlayClassName={s.overlay}
-    >
-      <button onClick={onRequestClose} className={s.closeBtn}>
-        Close
-      </button>
-      {image && <img src={image.urls.full} alt={image.alt_description} />}
+    <Modal isOpen={isOpen} onRequestClose={onRequestClose} className={s.modal}>
+      <div className={s.modalContent}>
+        <img
+          src={urls.regular}
+          alt={image.alt_description}
+          className={s.modalImage}
+        />
+        <div className={s.info}>
+          <p>
+            <strong>Author:</strong> {user.name}
+          </p>
+          <p>
+            <strong>Likes:</strong> {likes}
+          </p>
+        </div>
+        <button onClick={onRequestClose} className={s.closeBtn}>
+          Close
+        </button>
+      </div>
     </Modal>
   );
 };
